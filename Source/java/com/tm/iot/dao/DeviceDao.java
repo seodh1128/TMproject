667ef2be437b445df1a2c846927ad01eabb55fab
SeoDongHyun <seodh1128@naver.com>
2017-11-29 13:09:55 +0900

    스프링 소스 분리

--- Source/java/com/tm/iot/dao/DeviceDao.java
+++
@@ -1,9 +0,0 @@
-package com.tm.iot.dao;
-
-import java.util.List;
-
-import com.tm.iot.model.Device;
-
-public interface DeviceDao {
-	List<Device> selectList();
-}
--- Source/java/com/tm/iot/service/DeviceService.java
+++
@@ -1,9 +0,0 @@
-package com.tm.iot.service;
-
-import java.util.List;
-
-import com.tm.iot.model.Device;
-
-public interface DeviceService {
-	List<Device> getList();
-}
