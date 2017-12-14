ece72cff76ec7f2e405279d4f253e9d1eb999f26
LLljs <hikoukigumo@naver.com>
2017-12-14 15:50:09 +0900

    트랜젝션 처리 추가
    @Transactional 어노테이션 추가

--- Source/SpringProject/java/com/tm/iot/service/IotDataServiceImpl.java
+++ Source/SpringProject/java/com/tm/iot/service/IotDataServiceImpl.java
@@ -2,6 +2,7 @@ package com.tm.iot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tm.iot.dao.IotDataDao;
 import com.tm.iot.model.IotData;
@@ -19,8 +20,8 @@ public class IotDataServiceImpl implements IotDataService {
 	@Autowired
 	private IotDataDao dao;
 
-
 	@Override
+	@Transactional
 	public boolean insert(IotData iotData) {
 		int result = dao.insert(iotData); 
 		return result == 1;
